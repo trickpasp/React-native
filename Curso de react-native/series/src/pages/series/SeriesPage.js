7f218e977cd13c5c8a70eac5a8d9def1c1da3138
Patrick <patrickeeep12@gmail.com>
2018-07-20 11:06:09 -0300

    Criando página séries

---
+++ Curso de react-native/series/src/pages/series/SeriesPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class SeriesPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <View>
+                <Text>
+                    Series Pages
+                </Text>
+            </View>   
+        );
+    }
+}
