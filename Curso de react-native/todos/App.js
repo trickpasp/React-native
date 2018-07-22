f41dc9e107e57628eb68aa9fcd123c98e01be5ab
Patrick <patrickeeep12@gmail.com>
2018-07-22 18:00:27 -0300

    Criando projeto todos

---
+++ Curso de react-native/todos/App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
