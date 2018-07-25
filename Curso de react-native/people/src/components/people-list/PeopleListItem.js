3fac9fd0e72100108049ba1009fc8dbb33da0678
Patrick <patrickeeep12@gmail.com>
2018-07-25 12:52:10 -0300

    Fazendo indentação correta

--- Curso de react-native/people/src/components/people-list/PeopleListItem.js
+++ Curso de react-native/people/src/components/people-list/PeopleListItem.js
@@ -7,16 +7,25 @@ import { capitalizerFirstLetter } from '../../util';
 
 const PeopleListItem = props => {
     const { people, navigatioToPeopleDetail } = props;
-    const { title ,first, last } = people.name;
-    const { thumbnail } = people.picture;
+    const { title ,first, last } = people.name;    
     return( 
         <TouchableOpacity onPress={() => {
             navigatioToPeopleDetail({ people });
         }}>       
             <View style={styles.line}>
-                    <Image style={styles.avatar} source={{ uri: thumbnail }} />
+                    <Image style={styles.avatar} source={{ uri: people.picture.thumbnail }} />
                     <Text  style={styles.lineText}>
-                        { `${ capitalizerFirstLetter(title) } ${ capitalizerFirstLetter(first) } ${ capitalizerFirstLetter(last) }` }
+                        {  
+                           `${ 
+                                capitalizerFirstLetter(title) 
+                            } 
+                            ${ 
+                                capitalizerFirstLetter(first) 
+                            } 
+                            ${ 
+                                capitalizerFirstLetter(last) 
+                            }` 
+                        }
 
 
                     </Text>
