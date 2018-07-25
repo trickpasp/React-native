6c61711c585217f669353f5199c512f0b5823819
Patrick <patrickeeep12@gmail.com>
2018-07-25 12:57:20 -0300

    Adicionando funções de seteditingtodo

--- Curso de react-native/todos/src/components/TodoList.js
+++ Curso de react-native/todos/src/components/TodoList.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import { connect } from 'react-redux';
 
 import TodoListItem from './TodoListItem';
-import { toggleTodo } from '../actions';
+import { toggleTodo, setEditingTodo } from '../actions';
 
 
-const TodoList = ({ todos, dispatchToggleTodo }) => (
-    <View>
+const TodoList = ({ todos, dispatchToggleTodo, dispatchSetEditingTodo }) => (
+    <View style={styles.containerLine}>
         {todos.map(todo => {
             const { id } = todo;
             return(      
@@ -16,14 +16,18 @@ const TodoList = ({ todos, dispatchToggleTodo }) => (
                     key={id} 
                     todo={todo}
                     onPressTodo={() => dispatchToggleTodo(id)}
+                    onLongPressTodo={() => dispatchSetEditingTodo(todo)}
                 />
         )}) }
     </View>
 );
 
-// const styles = StyleSheet.create({
-
-// });
+const styles = StyleSheet.create({
+    containerLine: {
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+});
 
 const mapStateToProps = state => {
     const { todos } = state;
@@ -32,5 +36,8 @@ const mapStateToProps = state => {
 
 export default connect(
     mapStateToProps, 
-    { dispatchToggleTodo: toggleTodo }
+    { 
+        dispatchToggleTodo: toggleTodo,
+        dispatchSetEditingTodo: setEditingTodo
+    }
 )(TodoList);
