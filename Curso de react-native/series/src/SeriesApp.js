5e65c0e78e94cc482d282df3c8fd8fc6071e9ed0
Patrick <patrickeeep12@gmail.com>
2018-07-26 07:40:36 -0300

    Aplicando redux à aplicação

--- Curso de react-native/series/src/SeriesApp.js
+++ Curso de react-native/series/src/SeriesApp.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Router from './Router';
 
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import rootReducer from  './reducers';
+
+const store = createStore(rootReducer, devToolsEnhancer());
 
 const SeriesApp = props => (
-    <Provider>
+    <Provider store={store}>
         <Router />
     </Provider>
 );
