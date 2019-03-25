653e72feb83891fa5db2603df89cdd0bff1aa7b0
Patrick Silva <patrickeeep12@gmail.com>
2019-03-25 03:12:59 -0300

    Criando campos de titulo e url da imagem. E Adicionando redux

--- Curso de react-native/series/src/pages/series/SerieFormPage/index.js
+++ Curso de react-native/series/src/pages/series/SerieFormPage/index.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TextInput 
+} from 'react-native';
 
-const SerieFormPage = props => (
+import { connect } from 'react-redux';
+
+import FormRow from '../../../components/form/FormRow';
+import { setField } from '../../../actions'
+
+const SerieFormPage = ({ serieForm, setField }) => (
     <View>
-        <Text>Test</Text>
+        <FormRow first>
+            <TextInput 
+                style={styles.input}
+                placeholder="Título"
+                value={serieForm.title}
+                onChangeText={value => setField('title', value)}
+            />
+        </FormRow>
+        <FormRow>
+            <TextInput 
+                style={styles.input}
+                placeholder="URL da imagem"
+                value={serieForm.img}
+                onChangeText={value => setField('img', value)}
+            />
+        </FormRow>
     </View>
 );
 
+const styles = StyleSheet.create({
+    input: {
+        paddingBottom: 5,
+        paddingLeft: 5,
+        paddingRight: 5,
+    }
+});
 
-export default SerieFormPage;
+const mapStateToProps = state => ({
+    serieForm: state.serieForm
+});
+
+const mapDispatchToProps = {
+    setField
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SerieFormPage);
