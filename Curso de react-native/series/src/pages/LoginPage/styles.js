602cdef476565396b6432f34846e4fc74143d254
Patrick Silva <patrickeeep12@gmail.com>
2019-03-25 04:30:40 -0300

    Reagrupando no novo caminho e adionando keyboardtype de email

---
+++ Curso de react-native/series/src/pages/LoginPage/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native'; 
+
+const styles = StyleSheet.create({
+    container: {
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+    input: {
+        paddingLeft: 5,
+        paddingLeft: 5,
+        paddingBottom: 5,
+    },
+    success: {
+        color: '#0dde00',
+    },
+    error: {
+        color: '#df0000',
+    },
+});
+
+export default styles;
