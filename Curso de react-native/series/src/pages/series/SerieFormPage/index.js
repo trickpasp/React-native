9f990047f951614aef3a98c298d1c574c4b855ab
Patrick Silva <patrickeeep12@gmail.com>
2019-03-25 01:05:09 -0300

    Criando SerieFormPage

---
+++ Curso de react-native/series/src/pages/series/SerieFormPage/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SerieFormPage = props => (
+    <View>
+        <Text>Test</Text>
+    </View>
+);
+
+
+export default SerieFormPage;
